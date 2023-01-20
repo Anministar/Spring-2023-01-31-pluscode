3cf5733c08b408ff7b1a4d6f4a347e840dd5c7a6
jungwoogyun <jwg8910@naver.com>
2023-01-20 13:24:03 +0900

    03

--- src/main/java/com/test/dto/PersonDto.java
+++ src/main/java/com/test/dto/PersonDto.java
@@ -1,5 +1,7 @@
 package com.test.dto;
 
+import org.springframework.stereotype.Component;
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -9,6 +11,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
+@Component
 public class PersonDto {
 	private String name;
 	private String age;
