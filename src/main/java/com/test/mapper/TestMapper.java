d5282b662b00828f604c0600c795b3a047b99a67
jungwoogyun <jwg8910@naver.com>
2023-01-27 15:08:02 +0900

    13-Mybatis

---
+++ src/main/java/com/test/mapper/TestMapper.java
@@ -0,0 +1,12 @@
+package com.test.mapper;
+
+import org.springframework.stereotype.Component;
+
+import org.springframework.stereotype.Component;
+
+ 
+public interface TestMapper {
+
+	//XML
+	public int countXML();
+}
---
+++ src/test/java/Mybatis/SqlSessionTests.java
@@ -0,0 +1,43 @@
+//package Mybatis;
+//
+//import java.sql.Connection;
+//
+//import org.apache.ibatis.session.SqlSession;
+//import org.apache.ibatis.session.SqlSessionFactory;
+//import org.junit.Test;
+//import org.junit.runner.RunWith;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.test.context.ContextConfiguration;
+//import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+//
+//import lombok.extern.log4j.Log4j;
+//
+//@RunWith(SpringJUnit4ClassRunner.class)
+//@ContextConfiguration("file:src/main/webapp/WEB-INF/spring/root-context.xml")
+//@Log4j
+//public class SqlSessionTests {
+//
+//	@Autowired
+//	SqlSessionFactory ssf;
+//	
+//	@Autowired
+//	SqlSessionTests session;
+//	
+//	@Test 
+//	public void func1() {
+//		log.info("SSF : "+ssf);
+//		SqlSessionTests dbs = ssf.openSession();
+//		Connection conn = dbs.getConnection();
+//		log.info("CONN : " + conn);
+//	}
+//	@Test 
+//	public void func2() {
+//		log.info("session : " + session);
+//		Connection conn = session.getConnection();
+//		log.info("CONN : " + conn);	
+//	}
+//}
+//
+//
+//
+//
